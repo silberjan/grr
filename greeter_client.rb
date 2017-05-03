bd02d57e4c2ecd328792048af55e842450369dd5
Jan Silbersiepe <jan.silbersiepe@motionintelligence.net>
2017-05-03 14:56:42 +0200

    added error code to second request

--- greeter_client.rb
+++ greeter_client.rb
@@ -1,36 +1,5 @@
 #!/usr/bin/env ruby
 
-# Copyright 2015, Google Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-#     * Redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above
-# copyright notice, this list of conditions and the following disclaimer
-# in the documentation and/or other materials provided with the
-# distribution.
-#     * Neither the name of Google Inc. nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-# Sample app that connects to a Greeter service.
-#
 # Usage: $ path/to/greeter_client.rb
 
 this_dir = File.expand_path(File.dirname(__FILE__))
@@ -46,13 +15,32 @@ def main
   stub = Helloworld::Greeter::Stub.new('localhost:50051', :this_channel_is_insecure)
 
   #check if CLI args were enterd. If not use 'world' as user name
-  user = ARGV.size > 0 ?  ARGV[0] : 'world'  
+  user = ARGV.size > 0 ?  ARGV[0] : 'world' 
 
-  message = stub.say_hello(Helloworld::HelloRequest.new(name: user)).message
-  p "Message form Server: #{message}"
 
-  message = stub.say_hello_again(Helloworld::HelloRequest.new(name: user)).message
-  p "Message from Server: #{message}"
+  begin
+    resp = stub.say_hello(Helloworld::HelloRequest.new(name: user))
+    p "Message form Server: #{resp.message}"
+  end 
+
+  begin
+    resp = stub.say_hello_again(Helloworld::HelloRequest.new(name: user))
+    p "Message from Server: #{resp.message}"
+  rescue GRPC::BadStatus => e
+    p "Error from Server. Code: #{e.code} Details: #{e.details}"
+  end
+
+  #  # stream
+  # begin
+  #   p "Starting BiDi request"
+  #   resps = stub.hello_stream(['hello','hello 2'])
+  #   p "BiDi resp: #{resps}"
+  #   resps.each { |r| p r }
+  #   rescue GRPC::BadStatus => e
+  #     p "Error from Server. Code: #{e.code} Details: #{e.details}"
+  # end
+
+
 end
 
 main
--- greeter_server.rb
+++ greeter_server.rb
@@ -1,36 +1,5 @@
 #!/usr/bin/env ruby
 
-# Copyright 2015, Google Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-#     * Redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above
-# copyright notice, this list of conditions and the following disclaimer
-# in the documentation and/or other materials provided with the
-# distribution.
-#     * Neither the name of Google Inc. nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-# Sample gRPC server that implements the Greeter::Helloworld service.
-#
 # Usage: $ path/to/greeter_server.rb
 
 this_dir = File.expand_path(File.dirname(__FILE__))
@@ -57,16 +26,26 @@ end
 class GreeterServer < Helloworld::Greeter::Service
 
   # say_hello implements the SayHello rpc method.
-  def say_hello(hello_req, _unused_call)
+  def say_hello(hello_req, _call)
     Log.log.info("hello requested for #{hello_req.name}")
-    sleep(3)
+    # Log.log.info("Metadata user is #{_call.metadata.user}")
+    # sleep(3)
     Log.log.info("#{hello_req.name} was greeted 😁")
     Helloworld::HelloReply.new(message: "Hello #{hello_req.name}")
   end
 
   # say_hello_again implements the SayHelloAgain rpc method.
   def say_hello_again(hello_req, _unused_call)
-    Helloworld::HelloReply.new(message: "Hello again #{hello_req.name}")
+    raise GRPC::BadStatus.new(GRPC::Core::StatusCodes::UNIMPLEMENTED, "Hello Again is not yet implemented")
+    # Helloworld::HelloReply.new(message: "Hello again #{hello_req.name}")
+  end
+
+  # hello_stream implements the HelloStream rpc method.
+  def hello_stream(requestStream, _call)
+    Log.log.info("Stream. params: #{requestStream}")
+    q = EnumeratorQueue.new(self)
+    return q.each_item
+    # requestStream.each { |r| Log.log.info("#{r}") }
   end
 
 end
