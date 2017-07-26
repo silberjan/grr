64b4399dfde756ea7c24ec60e412bead655e971c
Jan Silbersiepe <jan.silbersiepe@motionintelligence.net>
2017-07-26 19:19:35 +0200

    added benchmark with login

---
+++ benchmarks/benchmark_grr_login.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+this_dir = File.expand_path(File.dirname(__FILE__))
+lib_dir = File.join(this_dir, '../lib')
+$LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
+
+require 'grr-client'
+require 'json'
+require 'dotenv/load'
+require_relative '../test/requests/grpc'
+
+def benchmark
+
+    execution_times, threads, *rest = ARGV
+    execution_times = execution_times.to_i
+    threads = threads.to_i
+
+    logger = Logger.new(STDOUT)
+
+    host = ENV["GRR_HOST"] || "localhost"
+    port = ENV["GRR_PORT"] || "6575"
+    sessionId = ENV["SESSION_ID"] || "6c0d9ced-13c7-482c-9e33-85ff633d4604"
+
+    client = Grr::Client.new(Host: host,Port: port)
+
+    logger.info "requesting login"
+    requestBuilder = RequestBuilder::Grpc.new client
+
+    resp = requestBuilder.loginRequest
+    json = JSON.parse(resp.body)
+    sessionId = json["id"]
+    logger.info "session id is #{sessionId}"
+
+    requestBuilder.concurrentSessionRequests sessionId,execution_times,threads
+
+end 
+ 
+benchmark
